a4538653b98e1f0fa1ce7d891fc683e3507d465e
panhabot <sinsovannmony18@kit.edu.kh>
2022-05-26 17:29:38 +0700

    edit product

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -5,4 +5,7 @@ module.exports = (app) => {
     app.use("/api/product", checkAuth, router);
 
     router.post("/create_product/", productController.product_create);
+    router.get("/query_product/", productController.product_query);
+    router.get("/query_product_by_id/:productId", productController.product_query_by_id);
+    router.post("/edit_product/:productId", productController.edit_product);
 };
