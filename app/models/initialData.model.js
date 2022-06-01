1c927d22c9b0b0de332f7dd9d99592d0c9a84523
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2022-06-01 14:08:02 +0700

    Update Initial Data

--- app/models/initialData.model.js
+++ app/models/initialData.model.js
@@ -6,46 +6,54 @@ exports.initial_product = async () => {
         const productExisting = await Product.findOne();
         //if not, create it.
         if (!productExisting) {
-            Product.create({
-                name: "iphone",
-                quantity: 5,
-                price: 1200,
-                categoryId: 1,
-                available: true,
-                description: "apple product that call iphone",
-                image: "image_url",
-                created_date: "01-01-2022",
-            });
-            Product.create({
-                name: "samsung",
-                quantity: 1,
-                price: 900,
-                categoryId: 1,
-                available: true,
-                description: "android product that call samsung",
-                image: "image_url",
-                created_date: "01-01-2022",
-            });
-            Product.create({
-                name: "Huawei",
-                quantity: 3,
-                price: 190,
-                categoryId: 1,
-                available: true,
-                description: "android product that call huawei",
-                image: "image_url",
-                created_date: "01-01-2022",
-            });
-            Product.create({
-                name: "Oppo",
-                quantity: 2,
-                price: 200,
-                categoryId: 1,
-                available: true,
-                description: "android product that call oppo",
-                image: "image_url",
-                created_date: "01-01-2022",
-            });
+            Product.create(
+                {
+                    name: 'Nike Air Max',
+                    quantity: 1,
+                    price: 120,
+                    categoryId: 1,
+                    available: true,
+                    description: 'Nike, Air Max, Fashion, Shoes',                      
+                    image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/skwgyqrbfzhu6uyeh0gg/air-max-270-mens-shoes-KkLcGR.png',
+                    created_date: "01-01-2022",
+                }
+            );
+            Product.create(
+                {
+                    name: 'iPhone 13 Mini',
+                    quantity: 1,
+                    price: 1360,
+                    categoryId: 1,
+                    available: true,
+                    description: 'Apple, iPhone, 13, Mini',                                            
+                    image: 'https://rewardmobile.co.uk/wp-content/uploads/2021/09/iPhone13_ProductImage_1000x1000_1.jpg',
+                    created_date: "01-01-2022",
+                }
+            );
+            Product.create(
+                {
+                    name: 'KOOMPI E13',
+                    quantity: 1,
+                    price: 270,
+                    categoryId: 1,
+                    available: true,
+                    description: 'KOOMPI, E13, Electronics',                                            
+                    image: 'https://konfulononline.com/image/cache/catalog/KOOMPI/KOOMPI%20E13/E13-RoseGold3-800px-800x800.png',
+                    created_date: "01-01-2022",
+                }
+            );
+            Product.create(
+                {
+                    name: 'PlayStation 5',
+                    quantity: 1,
+                    price: 505,
+                    categoryId: 1,
+                    available: true,
+                    description: 'PlayStation, 5, Red Dragon',                                            
+                    image: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2021%2F09%2Fsony-playstation-5-pro-release-rumors-info-000.jpg?w=960&cbr=1&q=90&fit=max',
+                    created_date: "01-01-2022",
+                }
+            );                        
         }
     } catch (error) {
         console.log(error.message);
